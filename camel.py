8bfbaba0f08b72c5ff5722560baa2daf8f72da89
flomartel <fjm25@kent.ac.uk>
2026-02-05 09:40:06 +0000

    ver2

--- camel.py
+++ camel.py
@@ -1,8 +1,12 @@
 
 camel = input("Enter a sentance in camel case: ").lower()
-snake = ""
+
 
 for upper in camel:
-    if upper = 
+    if upper.isupper():
+        camel = camel.replace (upper, "_" + upper.lower())
+
+print (camel)
+
 
 
