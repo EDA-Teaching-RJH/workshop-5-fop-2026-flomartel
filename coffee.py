12085ca3d08ca7a92996ea155b8381e644759312
flomartel <fjm25@kent.ac.uk>
2026-02-05 10:25:08 +0000

    ver3

--- coffee.py
+++ coffee.py
@@ -3,8 +3,8 @@ amount_due = 75
 print ("Amount Due: 75p")
 
 
-coin = int(input("Please insert a coin(50, 20, 10 or 5): "))
+coin = int(input("Please insert a coin (50, 20, 10 or 5): "))
 
 if coin == 5 or coin == 10 or coin == 20 or coin == 50:
     amount_due -= coin
-    print ("Amount still due: ", amount_due)
+    print ("Amount still due:",amount_due)
